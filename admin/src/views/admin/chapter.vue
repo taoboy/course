892edd59f039e4b66f032ce0c3ae32ad223bf715
ouyangbo <1196526952@qq.com>
2020-04-27 17:48:03 +0800

    1.点击sidebar菜单实现页面跳转并增加激活样式

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -973,3 +973,15 @@
     </table>
 
 </template>
+<script>
+    export default {
+        name: 'chapter',
+        mounted:function(){
+            this.$parent.activeSidebar("business-chapter-sidebar")
+
+        },
+        methods: {
+
+        }
+    }
+</script>
--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -3,3 +3,16 @@
         <h1>你好，欢迎您</h1>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'welcome',
+        mounted:function(){
+            this.$parent.activeSidebar("welcome-sidebar")
+
+        },
+        methods: {
+
+        }
+    }
+</script>
