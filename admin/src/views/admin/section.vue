84c56b979348f2054d40116fe053f59bcccd7875
ouyangbo <1196526952@qq.com>
2020-05-12 12:57:56 +0800

    1.生成器开发：增加生成vue界面代码。 2.小节管理增删改查功能测试成功

---
+++ admin/src/views/admin/section.vue
@@ -0,0 +1,245 @@
+<template>
+    <div>
+        <p>
+            <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+                <i class="ace-icon fa fa-edit"></i>
+                添加
+            </button>
+            &nbsp;
+            <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-refresh"></i>
+            刷新
+        </button>
+
+        </p>
+
+        <!--第二步：放在需要显示的地方
+            v-bind:list="list",前面的list，
+            是分页组件暴露出来的一个回调方法，后面的list，是section组件的list方法。
+        -->
+        <pagination ref="pagination" v-bind:list="list"></pagination>
+
+        <table id="simple-table" class="table  table-bordered table-hover">
+        <thead>
+        <tr>
+                <th>id</th>
+
+                <th>标题</th>
+
+                <th>课程</th>
+
+                <th>大章</th>
+
+                <th>视频</th>
+
+                <th>时长</th>
+
+                <th>收费</th>
+
+                <th>顺序</th>
+
+                <th>创建时间</th>
+
+                <th>修改时间</th>
+
+
+        </tr>
+        </thead>
+
+        <tbody>
+
+        <tr v-for="section in sections">
+                <td>{{section.id}}</td>
+                <td>{{section.title}}</td>
+                <td>{{section.courseId}}</td>
+                <td>{{section.chapterId}}</td>
+                <td>{{section.video}}</td>
+                <td>{{section.time}}</td>
+                <td>{{section.charge}}</td>
+                <td>{{section.sort}}</td>
+                <td>{{section.createdAt}}</td>
+                <td>{{section.updatedAt}}</td>
+
+            <td>
+                <div class="hidden-sm hidden-xs btn-group">
+                    <button v-on:click="edit(section)" class="btn btn-xs btn-info">
+                        <i class="ace-icon fa fa-pencil bigger-120"></i>
+                    </button>
+
+                    <button v-on:click="del(section.id)" class="btn btn-xs btn-danger">
+                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                    </button>
+                </div>
+            </td>
+        </tr>
+
+
+
+        </tbody>
+    </table>
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="form-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">表单</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form class="form-horizontal">
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">标题</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.title" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">课程</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.courseId" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">大章</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.chapterId" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">视频</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.video" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">时长</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.time" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">收费</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.charge" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">顺序</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.sort" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">创建时间</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.createdAt" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">修改时间</label>
+                                    <div class="col-sm-10">
+                                        <input v-model="section.updatedAt" class="form-control">
+                                    </div>
+                                </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                        <button type="button" class="btn btn-primary" v-on:click="save">保存</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+    //第一步：引入组件
+    import Pagination from "../../components/pagination";
+    export default {
+        name: 'section',
+        components: {Pagination},
+        data:function(){
+            return {
+                section:{},
+                sections:[]
+            }
+        },
+
+        mounted:function(){
+            //this.$parent.activeSidebar("business-section-sidebar")
+            let _this = this;
+            _this.$refs.pagination.size=5;
+            _this.list(1);
+        },
+        methods: {
+            add(){
+                let _this = this;
+                _this.section={};
+                $("#form-modal").modal("show");
+            },
+
+            edit(section){
+                let _this = this;
+                _this.section =  $.extend({},section);
+                $("#form-modal").modal("show");
+            },
+
+            list(page){
+                let _this = this;
+                Loading.show();
+                _this.$ajax.post(process.env.VUE_APP_SERVER  + '/business/admin/section/list',{
+                    page:page,
+                    size:_this.$refs.pagination.size,
+                })
+                    .then((response) => {
+                        Loading.hide();
+
+                        let resp = response.data;
+                        _this.sections = resp.content.list;
+                        _this.$refs.pagination.render(page,resp.content.total)
+                    })
+            },
+
+            save(){
+                let _this = this;
+
+                // 保存校验
+
+
+                Loading.show();
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/section/save',_this.section)
+                    .then((response) => {
+                        Loading.hide();
+
+                        let resp = response.data;
+                        if (resp.success){
+                            $("#form-modal").modal("hide");
+                            _this.list(1);
+                            Toast.success("保存成功！");
+                        }else {
+                            Toast.warning(resp.message)
+                        }
+                    })
+            },
+
+            del(id){
+                let _this = this;
+                Confirm.show("删除小节后不可恢复，确认删除!",function () {
+                    Loading.show();
+                    _this.$ajax.delete(process.env.VUE_APP_SERVER  + '/business/admin/section/delete/' + id)
+                        .then((response) => {
+                            Loading.hide();
+                            let resp = response.data;
+                            if (resp.success){
+                                _this.list(1);
+                                Toast.success("删除成功")
+                            }
+                        })
+                });
+            }
+        }
+    }
+</script>
