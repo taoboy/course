3004a62f54fff76cb9dd94f41c1bef1518014236
ouyangbo <1196526952@qq.com>
2020-05-29 00:22:16 +0800

    1.利用setInterval完成自动保存功能 2。增加js日期格式化方法

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -177,6 +177,11 @@
                         <form class="form-horizontal">
                             <div class="form-group">
                                 <div class="col-lg-12">
+                                    {{saveContentLabel}}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-lg-12">
                                     <div id="content"></div>
                                 </div>
                             </div>
@@ -214,6 +219,7 @@
                 COURSE_STATUS: COURSE_STATUS,
                 categorys:[],
                 tree:{},
+                saveContentLabel:"",
             }
         },
 
@@ -382,6 +388,7 @@
 
                 // 先清空历史文本
                 $("#content").summernote('code', '');
+                _this.saveContentLabel = "";
                 Loading.show();
                 _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course/find-content/' + id).then((response)=>{
                     Loading.hide();
@@ -393,6 +400,14 @@
                             $("#content").summernote('code', resp.content.content);
                         }
 
+                        //定时自动保存
+                        let saveContentInterval = setInterval(function () {
+                            _this.saveContent();
+                        },5000);
+                        //关闭内容框时，清空自动保存任务
+                        $("#course-content-modal").on('hidden.bs.modal',function (e) {
+                            clearInterval(saveContentInterval);
+                        })
                     } else {
                         Toast.warning(resp.message);
                     }
@@ -401,7 +416,6 @@
 
             saveContent () {
                 let _this = this;
-                alert(_this.course)
                 let content = $("#content").summernote("code");
                 _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course/save-content', {
                     id: _this.course.id,
@@ -410,12 +424,17 @@
                     Loading.hide();
                     let resp = response.data;
                     if (resp.success) {
-                        Toast.success("内容保存成功");
+                        // Toast.success("内容保存成功");
+                        let now = Tool.dateFormat("mm:ss");
+                        _this.saveContentLabel = "最后保存时间：" + now;
                     } else {
                         Toast.warning(resp.message);
                     }
                 });
             },
+
+
+
         }
     }
 </script>
