65e3910b3df948e4665db118d08dbe26aa00ae16
ouyangbo <1196526952@qq.com>
2020-06-07 02:45:53 +0800

    1.文件上传功能开发，讲师头像显示优化

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -91,9 +91,14 @@
                                       </div>
                                       <div class="form-group">
                                           <label class="col-sm-2 control-label">头像</label>
-                                          <div class="col-sm-10">
+                                          <div class="col-sm-4">
                                               <input type="file" id="file-upload-input" v-on:change="uploadImage()" >
-                                              <img v-bind:src="teacher.image" class="img-responsive"/>
+                                              <div v-show="teacher.image" class="row">
+                                                  <div class="col-md-4">
+                                                      <img v-bind:src="teacher.image" class="img-responsive"/>
+                                                  </div>
+                                              </div>
+
                                           </div>
                                       </div>
                                       <div class="form-group">
