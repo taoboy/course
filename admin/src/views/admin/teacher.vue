71f7165dd0d0838e94f33b0e79ac7581edadd2aa
ouyangbo <1196526952@qq.com>
2020-06-09 23:22:24 +0800

    1.文件上传功能开发，制作文件上传公共组件 2.修复不能连续上传同一个文件的BUG

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -91,18 +91,16 @@
                               </div>
                               <div class="form-group">
                                   <label class="col-sm-2 control-label">头像</label>
-                                  <div class="col-sm-4">
-                                      <button type=button v-on:click="selectImage()" class="btn btn-white btn-default btn-round">
-                                          <i class="ace-icon fa fa-upload"></i>
-                                          上传头像
-                                      </button>
-                                      <input class="hidden" type="file" ref="file" id="file-upload-input" v-on:change="uploadImage()" >
+                                  <div class="col-sm-10">
+                                      <file v-bind:text="'上传头像'"
+                                            v-bind:inputId="'image-upload'"
+                                            v-bind:after-upload="afterUpload"
+                                            v-bind:suffixs="['jpg', 'jpeg', 'png']"></file>
                                       <div v-show="teacher.image" class="row">
                                           <div class="col-md-4">
                                               <img v-bind:src="teacher.image" class="img-responsive"/>
                                           </div>
                                       </div>
-
                                   </div>
                               </div>
                               <div class="form-group">
@@ -139,9 +137,10 @@
 <script>
     //第一步：引入组件
     import Pagination from "../../components/pagination";
+    import File from "../../components/file";
     export default {
         name: 'business-teacher',
-        components: {Pagination},
+        components: {File, Pagination},
         data:function(){
             return {
                 teacher:{},
@@ -232,44 +231,12 @@
                         })
                 });
             },
-            uploadImage(){
+            afterUpload(resp){
                 let _this = this;
-                let formData = new window.FormData();
-                let  file = _this.$refs.file.files[0];
-
-
-                // 判断文件格式
-                let suffixs = ["jpg","jpeg","png"]
-                let fileName = file.name;
-                let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
-                let validateSuffix = false;
-                for (let i = 0; i < suffixs.length; i++) {
-                    if (suffixs[i].toLowerCase() === suffix) {
-                        validateSuffix = true;
-                        break;
-                    }
-                }
-                if (!validateSuffix) {
-                    Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
-                    $("#" + _this.inputId + "-input").val("");
-                    return;
-                }
-
-                // key："file"必须和后端controller参数名一致
-
-                formData.append('file',document.querySelector('#file-upload-input').files[0])
-                Loading.show();
-                _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload',formData).then((response)=>{
-                    Loading.hide();
-                    let resp = response.data;
-                    let  image = resp.content;
-                    console.log("头像地址：" + image);
-                    _this.teacher.image = image;
-                });
-            },
-            selectImage(){
-                $("#file-upload-input").trigger("click");
+                let image = resp.content;
+                _this.teacher.image = image;
             }
+
         }
     }
 </script>
