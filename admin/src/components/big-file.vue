a6dfefa3eed0af8fb1897f9fbff33b51f05e26cc
ouyangbo <1196526952@qq.com>
2020-06-25 23:30:05 +0800

    1.大文件断点续传与极速秒传：增加big-file组件，分片传输测试成功

---
+++ admin/src/components/big-file.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+      <button type=button v-on:click="selectFile()" class="btn btn-white btn-default btn-round">
+          <i class="ace-icon fa fa-upload"></i>
+          {{text}}
+      </button>
+      <input class="hidden" type="file" ref="file" v-bind:id="inputId+'-input'" v-on:change="uploadFile()" >
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'big-file',
+    //props，用于父子组件传递数据，可以理解成组件可配置的属性
+    props: {
+      text:{
+          default: "上传大文件"
+      },
+        inputId:{
+          default: "file-upload"
+      },
+        suffixs:{
+          default: []
+      },
+      use:{
+          default:""
+      },
+      afterUpload: {
+        type: Function,
+        default: null
+      },
+
+      afterUploadContentFile:{
+          type: Function,
+          default: null
+      }
+    },
+    data: function () {
+      return {
+      }
+    },
+    methods: {
+        uploadFile(){
+            let _this = this;
+            let formData = new window.FormData();
+            let file = _this.$refs.file.files[0];
+
+
+            // 判断文件格式
+            let suffixs = _this.suffixs;
+            let fileName = file.name;
+            let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
+            let use = _this.use
+            let validateSuffix = false;
+
+            for (let i = 0; i < suffixs.length; i++) {
+                if (suffixs[i].toLowerCase() === suffix) {
+                    validateSuffix = true;
+                    break;
+                }
+            }
+            if (!validateSuffix) {
+                Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
+                $("#" + _this.inputId + "-input").val("");
+                return;
+            }
+
+            //文件分片
+            let shardSize = 20 * 1024 * 1024; //以20MB为一个分片
+            let shardIndex = 1; //分片索引
+            let start = shardIndex * shardSize; //当前分片起始位置 0 20 40
+            // let end = start + shardSize; //分片结束的位置 1， 20~40 35
+            let end = Math.min(file.size,start + shardSize); //当前分片结束位置
+            let fileShard = file.slice(start,end); //从文件中截取当前的分片数据
+
+            // key："file"必须和后端controller参数名一致
+
+            formData.append('file',fileShard);
+            formData.append('use',use);
+            Loading.show();
+            _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload',formData).then((response)=>{
+                Loading.hide();
+                let resp = response.data;
+                console.log("上传文件成功：" + resp);
+                _this.afterUpload(resp);
+                $("#" + _this.inputId + "-input").val("");
+            });
+        },
+
+        selectFile () {
+            let _this = this;
+            $("#" + _this.inputId + "-input").trigger("click");
+        }
+    }
+  }
+</script>
--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -98,12 +98,12 @@
                                     <div class="form-group">
                                         <label class="col-sm-2 control-label">视频</label>
                                             <div class="col-sm-10">
-                                                <file v-bind:text="'上传视频'"
+                                                <big-file v-bind:text="'上传大视频'"
                                                       v-bind-id="'video-upload'"
                                                       v-bind:after-upload="afterUpload"
                                                       v-bind:suffixs="['mp4']"
                                                       v-bind:use="FILE_USE.COURSE.key"
-                                                ></file>
+                                                ></big-file>
                                                 <div v-show="section.video" class="row">
                                                     <div class="col-md-9">
                                                         <video v-bind:src="section.video" id="video" controls="controls"></video>
@@ -149,10 +149,11 @@
     //第一步：引入组件
     import Pagination from "../../components/pagination";
     import File from "../../components/file";
+    import BigFile from "../../components/big-file";
 
     export default {
         name: 'business-section',
-        components: {Pagination,File},
+        components: {Pagination,File,BigFile},
         data:function(){
             return {
                 section:{},
@@ -263,11 +264,13 @@
             /**
              * 获取时长
              */
-            getTime(){
+            getTime() {
                 let _this = this;
-                let  ele = document.getElementById("video");
-                _this.section.time = parseInt(ele.duration,10);
-            }
+                setTimeout(function () {
+                    let ele = document.getElementById("video");
+                    _this.section.time = parseInt(ele.duration, 10);
+                }, 1000);
+            },
         }
     }
 </script>
