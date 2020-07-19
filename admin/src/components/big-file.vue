0d44885a5b8434e6d502ef79b3dbf2564f8ed575
ouyangbo <1196526952@qq.com>
2020-07-19 15:31:23 +0800

    1.大文件断点续传与极速秒传：上传分片之前先进行分片检查，支持断点续传

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -85,7 +85,7 @@
             }
 
             //文件分片
-            let shardSize = 40 * 1024 * 1024; //以20MB为一个分片
+            let shardSize = 10 * 1024 * 1024; //以20MB为一个分片
             let shardIndex = 1; //分片索引
 
             let size = file.size;
@@ -104,7 +104,32 @@
                 'key': key62
             };
 
-            _this.upload(param);
+            _this.check(param);
+        },
+
+        /**
+         * 检查文件状态，是否已上传过？传到第几个分片？
+         */
+        check (param) {
+            let _this = this;
+            _this.$ajax.get(process.env.VUE_APP_SERVER + '/file/admin/check/' + param.key).then((response)=>{
+                let resp = response.data;
+                if (resp.success) {
+                      let obj = resp.content;
+                      if (!obj) {
+                            param.shardIndex = 1;
+                            console.log("没有找到文件记录，从分片1开始上传");
+                            _this.upload(param);
+                          } else {
+                            param.shardIndex = obj.shardIndex + 1;
+                            console.log("找到文件记录，从分片" + param.shardIndex + "开始上传");
+                            _this.upload(param);
+                          }
+                    } else {
+                      Toast.warning("文件上传失败");
+                      $("#" + _this.inputId + "-input").val("");
+                    }
+            })
         },
 
         upload: function (param) {
@@ -123,7 +148,8 @@
                 Loading.show();
                 _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', param).then((response) => {
                     Loading.hide();
-                    let resp = response.data;
+
+                   let resp = response.data;
                     console.log("上传文件成功：" + resp);
                     if (shardIndex < shardTotal) {
                         //上传下一个分片
