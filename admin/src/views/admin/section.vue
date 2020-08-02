64df728b347f875a7a1d2b9603e48da386b2f9e0
ouyangbo <1196526952@qq.com>
2020-08-02 17:03:37 +0800

    1.视频加密与授权播放：获取vod授权码并授权播放，需要在存储管理中把权限设置成公共读

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -26,7 +26,7 @@
             <tr>
                 <th>ID</th>
                 <th>标题</th>
-                <th>视频</th>
+                <th>VOD</th>
                 <th>时长</th>
                 <th>收费</th>
                 <th>顺序</th>
@@ -38,12 +38,15 @@
             <tr v-for="section in sections">
                 <td>{{section.id}}</td>
                 <td>{{section.title}}</td>
-                <td>{{section.video}}</td>
+                <td>{{section.vod}}</td>
                 <td>{{section.time | formatSecond}}</td>
                 <td>{{SECTION_CHARGE | optionKV(section.charge)}}</td>
                 <td>{{section.sort}}</td>
                 <td>
                     <div class="hidden-sm hidden-xs btn-group">
+                        <button v-on:click="play(section)" class="btn btn-xs btn-info">
+                            <i class="ace-icon fa fa-video-camera bigger-120"></i>
+                        </button>
                         <button v-on:click="edit(section)" class="btn btn-xs btn-info">
                             <i class="ace-icon fa fa-pencil bigger-120"></i>
                         </button>
@@ -92,8 +95,10 @@
                                          v-bind:use="FILE_USE.COURSE.key"
                                          v-bind:after-upload="afterUpload"></vod>
                                     <div v-show="section.video" class="row">
+                                        <player v-bind:player-id="'form-player-div'"
+                                                ref="player"></player>
                                         <div class="col-md-9">
-                                            <video v-bind:src="section.video" id="video" controls="controls"></video>
+                                            <video v-bind:src="section.video" id="video" controls="controls" hidden></video>
                                         </div>
                                     </div>
                                 </div>
@@ -139,6 +144,7 @@
                 </div><!-- /.modal-content -->
             </div><!-- /.modal-dialog -->
         </div><!-- /.modal -->
+        <modal-player ref="modalPlayer"></modal-player>
     </div>
 </template>
 
@@ -146,8 +152,10 @@
     import Pagination from "../../components/pagination";
     import BigFile from "../../components/big-file";
     import Vod from "../../components/vod";
+    import Player from "../../components/player";
+    import ModalPlayer from "../../components/modal-player";
     export default {
-        components: {Pagination, BigFile, Vod},
+        components: {Player, Pagination, BigFile, Vod,ModalPlayer},
         name: "business-section",
         data: function() {
             return {
@@ -270,6 +278,7 @@
                 _this.section.video = video;
                 _this.section.vod = vod;
                 _this.getTime();
+                _this.$refs.player.playUrl(video);
             },
 
             /**
@@ -284,6 +293,14 @@
                     console.log(_this.section.time);
                 }, 1000);
             },
+            /**
+             * 播放视频
+             * @param section
+             */
+            play(section) {
+                let _this = this;
+                _this.$refs.modalPlayer.playVod(section.vod);
+            }
         }
     }
 </script>
