c1f941fb658f63864b5e799e5e0e92aadd7c4338
ouyangbo <1196526952@qq.com>
2020-08-03 22:40:45 +0800

    1.用户管理与登录：增加记住登录信息功能，安全加固，本地缓存保存密码密文

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -100,8 +100,14 @@
         methods: {
             login(){
                 let _this = this;
-                let passwordShow = _this.user.password;
-                _this.user.password = hex_md5(_this.user.password + KEY);
+                //let passwordShow = _this.user.password;
+
+                //如果密码是从缓存带来的，则不需要重新加密
+                let md5 = hex_md5(_this.user.password)
+                let rememberUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER) || {};
+                if (md5 != rememberUser.md5){
+                    _this.user.password = hex_md5(_this.user.password + KEY);
+                }
                 Loading.show();
                 _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login',_this.user)
                     .then((response) => {
@@ -112,15 +118,19 @@
                             let loginUser = resp.content;
                             Tool.setLoginUser(resp.content)
                             if (_this.remember) {
-                                //如果勾选记住我，则将用户密码保存到本地缓存，这里需要保存密码明文，否则登录时又会再加一层密
+                                //如果勾选记住我，则将用户密码保存到本地缓存，
+                                // 原：这里需要保存密码明文，否则登录时又会再加一层密
+                                //新：这里保存密码密文，并保存密文md5,用于检测密码是否被重新输入过
+                                let md5 = hex_md5(_this.user.password)
                                 LocalStorage.set(LOCAL_KEY_REMEMBER_USER,{
                                     loginName: loginUser.loginName,
-                                    password: passwordShow
+                                    password: _this.user.password,
+                                    md5:md5
                                 });
 
                             }else {
                                 //没有勾选“记住我”，要把本地缓存清空，否则按照mounted的逻辑，下次打开时会自动显示用户名密码
-                                LocalStorage.set("loginUser",null);
+                                LocalStorage.set(LOCAL_KEY_REMEMBER_USER,null);
                             }
 
                             _this.$router.push("/welcome")
