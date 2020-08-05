2678a0be1031b89fd90047cb8edcab16f608d97e
ouyangbo <1196526952@qq.com>
2020-08-05 21:03:21 +0800

    1.用户管理与登录：基于vue路由的登录拦截

--- admin/src/main.js
+++ admin/src/main.js
@@ -20,15 +20,35 @@ axios.interceptors.request.use(function (response) {
     console.log("请求：", response);
     return response;
 }, error => {});
+
+// 全局过滤器
+Object.keys(filter).forEach(key => {
+    Vue.filter(key, filter[key])
+});
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+    // 要不要对meta.loginRequire属性做监控拦截
+    if (to.matched.some(function (item) {
+        return item.meta.loginRequire
+    })) {
+        let loginUser = Tool.getLoginUser();
+        if (Tool.isEmpty(loginUser)) {
+            next('/login');
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
 new Vue({
    router,
    render: h => h(App),
 }).$mount('#app');
 
 
-// 全局过滤器
-Object.keys(filter).forEach(key => {
-    Vue.filter(key, filter[key])
-});
+
+
+
 
 console.log("环境：,", process.env.NODE_ENV);
