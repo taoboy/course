4f120d6f52d7457be8fd4c2480afc26e6e51d5a2
ouyangbo <1196526952@qq.com>
2020-08-09 00:54:07 +0800

    1.通用权限管理：点击【关联资源】按钮时，加载资源树

--- admin/src/views/admin/role.vue
+++ admin/src/views/admin/role.vue
@@ -20,39 +20,39 @@
         <pagination ref="pagination" v-bind:list="list"></pagination>
 
         <table id="simple-table" class="table  table-bordered table-hover">
-        <thead>
-        <tr>
-        <th>id</th>
-        <th>角色</th>
-        <th>描述</th>
-        <th>操作</th>
-        </tr>
-        </thead>
+            <thead>
+            <tr>
+            <th>id</th>
+            <th>角色</th>
+            <th>描述</th>
+            <th>操作</th>
+            </tr>
+            </thead>
 
-        <tbody>
+            <tbody>
 
-        <tr v-for="role in roles">
-                <td>{{role.id}}</td>
-                <td>{{role.name}}</td>
-                <td>{{role.desc}}</td>
+                <tr v-for="role in roles">
+                        <td>{{role.id}}</td>
+                        <td>{{role.name}}</td>
+                        <td>{{role.desc}}</td>
 
-            <td>
-                <div class="hidden-sm hidden-xs btn-group">
-                    <button v-on:click="edit(role)" class="btn btn-xs btn-info">
-                        <i class="ace-icon fa fa-pencil bigger-120"></i>
-                    </button>
+                    <td>
+                        <div class="hidden-sm hidden-xs btn-group">
+                            <button v-on:click="editResource(role)" class="btn btn-xs btn-info">
+                                <i class="ace-icon fa fa-list bigger-120"></i>
+                            </button>
+                            <button v-on:click="edit(role)" class="btn btn-xs btn-info">
+                                <i class="ace-icon fa fa-pencil bigger-120"></i>
+                            </button>
 
-                    <button v-on:click="del(role.id)" class="btn btn-xs btn-danger">
-                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                    </button>
-                </div>
-            </td>
-        </tr>
-
-
-
-        </tbody>
-    </table>
+                            <button v-on:click="del(role.id)" class="btn btn-xs btn-danger">
+                                <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
 
 
         <!-- Modal -->
@@ -65,19 +65,18 @@
                     </div>
                     <div class="modal-body">
                         <form class="form-horizontal">
-
-                                      <div class="form-group">
-                                          <label class="col-sm-2 control-label">角色</label>
-                                          <div class="col-sm-10">
-                                              <input v-model="role.name" class="form-control">
-                                          </div>
-                                      </div>
-                                      <div class="form-group">
-                                          <label class="col-sm-2 control-label">描述</label>
-                                          <div class="col-sm-10">
-                                              <input v-model="role.desc" class="form-control">
-                                          </div>
-                                      </div>
+                              <div class="form-group">
+                                  <label class="col-sm-2 control-label">角色</label>
+                                  <div class="col-sm-10">
+                                      <input v-model="role.name" class="form-control">
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="col-sm-2 control-label">描述</label>
+                                  <div class="col-sm-10">
+                                      <input v-model="role.desc" class="form-control">
+                                  </div>
+                              </div>
                         </form>
                     </div>
                     <div class="modal-footer">
@@ -87,6 +86,30 @@
                 </div>
             </div>
         </div>
+
+        <div id="resource-modal" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">角色资源关联配置</h4>
+                    </div>
+                    <div class="modal-body">
+                        <ul id="tree" class="ztree"></ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-white btn-default btn-round" data-dismiss="modal">
+                            <i class="ace-icon fa fa-times"></i>
+                            关闭
+                        </button>
+                        <button type="button" class="btn btn-white btn-info btn-round" v-on:click="saveResource()">
+                            <i class="ace-icon fa fa-plus blue"></i>
+                            保存
+                        </button>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
     </div>
 
 </template>
@@ -101,6 +124,8 @@
             return {
                 role:{},
                 roles:[],
+                resources: [],
+                zTree: {},
             }
         },
 
@@ -151,8 +176,6 @@
                 ) {
                     return;
                 }
-
-
                 Loading.show();
                 _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/role/save',_this.role)
                     .then((response) => {
@@ -183,7 +206,51 @@
                             }
                         })
                 });
-            }
+            },
+            editResource(role){
+                let _this = this;
+                _this.role = $.extend({}, role);
+                _this.loadResource();
+                $("#resource-modal").modal("show");
+            },
+
+            /**
+             * 加载资源树
+             */
+            loadResource() {
+                let _this = this;
+                Loading.show();
+                _this.$ajax.get(process.env.VUE_APP_SERVER + '/system/admin/resource/load-tree').then((res)=>{
+                    Loading.hide();
+                    let response = res.data;
+                    _this.resources = response.content;
+                    // 初始化树
+                    _this.initTree();
+                })
+            },
+
+            /**
+             * 初始资源树
+             */
+            initTree() {
+                let _this = this;
+                let setting = {
+                    check: {
+                        enable: true
+                    },
+                    data: {
+                        simpleData: {
+                            idKey: "id",
+                            pIdKey: "parent",
+                            rootPId: "",
+                            enable: true
+                        }
+                    }
+                };
+
+                _this.zTree = $.fn.zTree.init($("#tree"), setting, _this.resources);
+                _this.zTree.expandAll(true);
+            },
         }
     }
 </script>
