edf8fb2eaca838d227ae30b5085ac84875a37b64
ouyangbo <1196526952@qq.com>
2020-08-09 13:39:22 +0800

    1.通用权限管理：登录时，读取当前登录用户所属的角色的所有资源

--- server/src/main/java/com/course/server/dto/LoginUserDto.java
+++ server/src/main/java/com/course/server/dto/LoginUserDto.java
@@ -1,6 +1,9 @@
 package com.course.server.dto;
 
 
+import java.util.HashSet;
+import java.util.List;
+
 public class LoginUserDto {
 
     /**
@@ -19,6 +22,21 @@ public class LoginUserDto {
     private String name;
 
 
+    /**
+     * 登录凭证
+     */
+    private String token;
+
+    /**
+     * 所有资源，用于前端界面控制
+     */
+    private List<ResourceDto> resources;
+
+    /**
+     * 所有资源中的请求，用于后端接口拦截
+     */
+    private HashSet<String> requests;
+
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("LoginUserDto{");
@@ -26,8 +44,26 @@ public class LoginUserDto {
         sb.append(", loginName='").append(loginName).append('\'');
         sb.append(", name='").append(name).append('\'');
         sb.append(", token='").append(token).append('\'');
+        sb.append(", resources=").append(resources);
+        sb.append(", requests=").append(requests);
         sb.append('}');
         return sb.toString();
+    }
+
+    public List<ResourceDto> getResources() {
+        return resources;
+    }
+
+    public void setResources(List<ResourceDto> resources) {
+        this.resources = resources;
+    }
+
+    public HashSet<String> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(HashSet<String> requests) {
+        this.requests = requests;
     }
 
     public String getToken() {
@@ -38,10 +74,6 @@ public class LoginUserDto {
         this.token = token;
     }
 
-    /**
-     * 登录凭证
-     */
-    private String token;
 
 
     public String getId() {
